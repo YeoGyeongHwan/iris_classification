c9b1c63561bbb60cdfbd61bec8fdff2bdfec2e36
Yeo KH <durudghks@naver.com>
2023-01-09 17:06:30 +0900

    get iris dataset

---
+++ code1.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import load_iris
+import pandas as pd
+import numpy as np
+
+iris = load_iris()
+
+iris_data = pd.DataFrame(data=np.c_[iris['data'], iris['target']], columns=iris['feature_names'] + ['target'])
+iris_data['target'] = iris_data['target'].map({0: "setosa", 1: "versicolor", 2: "virginica"})
+
+iris_data.to_csv("iris_dataset.csv")
